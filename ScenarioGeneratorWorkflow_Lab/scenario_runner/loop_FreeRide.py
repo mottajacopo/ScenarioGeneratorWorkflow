c0d8ef2407ad5567020c1ca6b3ef1ce9622d591c
paperinfo <mottajacopo@gmail.com>
2021-03-13 12:12:18 +0100

    modifiche pc mio

--- ScenarioGeneratorWorkflow_Lab/scenario_runner/loop_FreeRide.py
+++ ScenarioGeneratorWorkflow_Lab/scenario_runner/loop_FreeRide.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.Popen("python loop_scenario_runner.py FreeRide C:\\Git\\ScenarioGeneratorWorkflow\\ScenarioGeneratorWorkflow_Lab\\scenariogeneration-main\\examples\\FreeRide\\xosc\\")
+subprocess.Popen("python loop_scenario_runner.py FreeRide C:\\Git\\ScenarioGeneratorWorkflow\\ScenarioGeneratorWorkflow_Lab\\scenariogeneration-main\\examples\\FreeRide\xosc\\")
